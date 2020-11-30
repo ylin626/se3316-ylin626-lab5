c0f9b6a0e6a7375060c1d717c1962ae1d18d1b75
ylin626 <ylin626@uwo.ca>
2020-11-30 12:22:08 -0500

    update header for bug by importing jwt
    fix the bug that the username can not be received

--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SearchComponent } from '../search/search.component';
+import { JwtHelperService } from "@auth0/angular-jwt";
 @Component({
   selector: 'app-head',
   templateUrl: './head.component.html',
@@ -11,8 +12,8 @@ export class HeadComponent implements OnInit {
 
   isLogin = false;
   userName="";
-  constructor(private http:HttpClient) {
-    this.userName = window.localStorage.getItem("userName")?window.localStorage.getItem("userName"):"Visitor";
+  constructor(private http:HttpClient,private jwt:JwtHelperService) {
+    this.userName = this.jwt.decodeToken(window.localStorage.getItem("token")).name?this.jwt.decodeToken(window.localStorage.getItem("token")).name:"Visitor";
     if (window.localStorage.getItem("isLogin") == "true") {
       this.isLogin = true;
     } else {
@@ -24,7 +25,7 @@ export class HeadComponent implements OnInit {
   }
   logout(){
     window.localStorage.removeItem("isLogin");
-    window.localStorage.removeItem("userName");
+    window.localStorage.removeItem("token");
     this.isLogin=false;
     this.userName="Visitor";
   }
